418bfefee834aa8b2997e6977a1d4f49b9da8ea5
Angello Falla <fallaangello@gmail.com>
2023-11-03 17:35:25 -0500

    Avance 2

--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,22 @@
 var lista = document.getElementById('nav-lista');
 var items = lista.getElementsByTagName('li');
 
+document.addEventListener('DOMContentLoaded', (event) => {
+    // get all elements with id "expand-btn"
+    var expandBtns = document.querySelectorAll('#expand-btn');
+    // add click event listener to each element
+    expandBtns.forEach(function(btn) {
+        btn.addEventListener('click', function() {
+            // remove class "expanded" from all elements
+            expandBtns.forEach(function(btn) {
+                btn.classList.remove('expanded');
+            });
+            // add class "expanded" to the clicked element
+            this.classList.add('expanded');
+        });
+    });
+});
+
 for (var i = 0; i < items.length; i++) {
     items[i].addEventListener('click', function() {
         var seleccionado = lista.getElementsByClassName('seleccionado');
