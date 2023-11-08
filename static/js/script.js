aa737b56103b075c9ebc3ffd60df9fc12f52b1da
Angello Falla <fallaangello@gmail.com>
2023-11-08 15:09:11 -0500

    Revision con bootstrap y responsivo

--- static/js/script.js
+++ static/js/script.js
@@ -1,28 +1,27 @@
-var lista = document.getElementById('nav-lista');
-var items = lista.getElementsByTagName('li');
+document.addEventListener("DOMContentLoaded", (event) => {
+	// get all elements with id "expand-btn"
+	var expandBtns = document.querySelectorAll("#expand-btn");
+	// add click event listener to each element
+	var lista = document.querySelector(".navbar-nav");
+	var items = lista.querySelectorAll(".nav-link");
 
-document.addEventListener('DOMContentLoaded', (event) => {
-    // get all elements with id "expand-btn"
-    var expandBtns = document.querySelectorAll('#expand-btn');
-    // add click event listener to each element
-    expandBtns.forEach(function(btn) {
-        btn.addEventListener('click', function() {
-            // remove class "expanded" from all elements
-            expandBtns.forEach(function(btn) {
-                btn.classList.remove('expanded');
-            });
-            // add class "expanded" to the clicked element
-            this.classList.add('expanded');
-        });
-    });
+	for (var i = 0; i < items.length; i++) {
+		items[i].addEventListener("click", function () {
+			var seleccionado = lista.querySelector(".seleccionado");
+			if (seleccionado) {
+				seleccionado.classList.remove("seleccionado");
+			}
+			this.classList.add("seleccionado");
+		});
+	}
+	expandBtns.forEach(function (btn) {
+		btn.addEventListener("click", function () {
+			// remove class "expanded" from all elements
+			expandBtns.forEach(function (btn) {
+				btn.classList.remove("expanded");
+			});
+			// add class "expanded" to the clicked element
+			this.classList.add("expanded");
+		});
+	});
 });
-
-for (var i = 0; i < items.length; i++) {
-    items[i].addEventListener('click', function() {
-        var seleccionado = lista.getElementsByClassName('seleccionado');
-        if (seleccionado.length > 0) {
-            seleccionado[0].className = seleccionado[0].className.replace('seleccionado', '');
-        }
-        this.className += 'seleccionado';
-    });
-}
