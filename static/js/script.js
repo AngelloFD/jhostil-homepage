e1a7a9fe4e64654c06dc2a110bdc53c9ed111046
Angello Falla <fallaangello@gmail.com>
2023-11-09 00:09:14 -0500

    Bootstrap 5 y responsive revision 2

--- static/js/script.js
+++ static/js/script.js
@@ -24,4 +24,39 @@ document.addEventListener("DOMContentLoaded", (event) => {
 			this.classList.add("expanded");
 		});
 	});
+
+	// Inicializa el índice de la tarjeta a mostrar
+	let cardIndex = 0;
+
+	// Obtiene los botones y el contenedor de las tarjetas
+	let nextButton = document.getElementById('nextButton');
+	let prevButton = document.getElementById('prevButton');
+	let cardContainer = document.getElementById('cardContainer');
+
+	// Función para actualizar la posición de las tarjetas
+	function updateCardPosition() {
+		let cardWidth = document.querySelector('.card').offsetWidth;
+		cardContainer.style.transform = `translateX(-${cardIndex * cardWidth}px)`;
+	}
+
+	// Función para manejar el click en el botón siguiente
+	nextButton.addEventListener('click', () => {
+		if (cardIndex < cardContainer.children.length - 3) {
+			cardIndex++;
+			updateCardPosition();
+		}
+	});
+
+	// Función para manejar el click en el botón anterior
+	prevButton.addEventListener('click', () => {
+		if (cardIndex > 0) {
+			cardIndex--;
+			updateCardPosition();
+		}
+	});
+
+	// Actualiza la posición de las tarjetas al cargar la página y cuando se redimensiona la ventana
+	window.addEventListener('load', updateCardPosition);
+	window.addEventListener('resize', updateCardPosition);
+
 });
